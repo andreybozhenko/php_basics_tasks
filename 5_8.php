91c3c758b9461c9939277e030a1a76be83f1b67a
andreybozhenko <35563628+andreybozhenko@users.noreply.github.com>
2018-01-19 12:08:25 +0200

    Update 5_8.php

--- 5_8.php
+++ 5_8.php
@@ -1,15 +1,15 @@
 <?php
 
-$age = "20";
-$a = "18";
-$b = "59";
+$age = "1";
+$minAge = "18";
+$maxAge = "59";
 
-if ($age >= $a && $age <= $b) {
-    echo "Вам еще работать и работать";
-} elseif ($age > $b) {
-    echo "Вам пора на пенсию";
-} elseif ($age >= 0 && $age <= 17) {
-    echo "Вам рано еще работать";
-} elseif ($age < 0) {
+if (is_int($age) || $age <= 0) {
     echo "Неизвестный возраст";
+} elseif ($age >= $minAge && $age <= $maxAge) {
+    echo "Вам еще работать и работать";
+} elseif ($age > $maxAge) {
+    echo "Вам пора на пенсию";
+} else {
+    echo "Вам рано еще работать";
 }
